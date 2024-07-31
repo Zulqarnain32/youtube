9b4a5816ecfd73df1636ea304ff7e73eeb4dffd3
Zulqarnain <zulqarnainchohan250@gmail.com>
2024-07-31 00:17:46 -0700

    added api key

--- src/utils/rapidapi.js
+++ src/utils/rapidapi.js
@@ -1,20 +1,21 @@
+
 import axios from "axios";
 
-const BASE_URL = "http://youtube138.p.rapidapi.com";
+const BASE_URL = 'https://youtube138.p.rapidapi.com'
 const options = {
   headers: {
-    'x-rapidapi-key': '4a58bf7c06msh18c876c79b2ef5ep156224jsn703cf5c925f0',
-    'x-rapidapi-host': 'youtube138.p.rapidapi.com'
-  }
+		'x-rapidapi-key': '4a58bf7c06msh18c876c79b2ef5ep156224jsn703cf5c925f0',
+		'x-rapidapi-host': 'youtube138.p.rapidapi.com'
+	}
 };
 
 export const fetchYoutubeData = async (url) => {
   try {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
     console.log(data);
-    return data;
+    return data
   } catch (err) {
-    console.log("Error fetching data:", err);
     console.log("Error fetching data:", err);
   }
 };
+
